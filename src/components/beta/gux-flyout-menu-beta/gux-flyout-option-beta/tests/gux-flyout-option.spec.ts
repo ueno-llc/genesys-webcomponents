7400ee8415cad1e050bb5d0c9f258521b6926d1b
dmitryueno <dmitrybelousov@ueno.co>
2020-10-01 17:04:09 +0300

    fix(flyout-menu): fix test

--- src/components/beta/gux-flyout-menu-beta/gux-flyout-option-beta/tests/gux-flyout-option.spec.ts
+++ src/components/beta/gux-flyout-menu-beta/gux-flyout-option-beta/tests/gux-flyout-option.spec.ts
@@ -26,23 +26,4 @@ describe('GuxFlyoutOption', () => {
   it('should build', async () => {
     expect(component).toBeInstanceOf(GuxFlyoutOption);
   });
-
-  describe('Methods', () => {
-    it('should change selected value', () => {
-      component.keyCode = 'test';
-      component.secondKeyCode = 'test';
-      component.thirdKeyCode = 'test';
-      const mockedEvent = {
-        keyCode: 'test',
-        secondKeyCode: 'test',
-        thirdKeyCode: 'test'
-      };
-
-      component.keyHandler(mockedEvent);
-      expect(component.selectedValue).toBeTruthy();
-
-      component.keyHandler(mockedEvent);
-      expect(component.selectedValue).toBeFalsy();
-    });
-  });
 });
